efe2b427ca72541f18a1a2a29dd56c07d1fb55b5
alcaina <j.alcaina.m@mail.com>
2015-05-06 15:22:30 +0200

    2nd update

--- src/simseed.R
+++ src/simseed.R
@@ -1,8 +1,14 @@
+#pooled sd function (needs number and sd vectors)
+pooledSd <- function(n, d) 
+{
+    return(sqrt(sum((n-1) * d^2) / sum(n - 1)))
+}
 
-
-simSeed<-function(N=100,n=10,b=0.05,ngen=1000,selq=0.2,sigma=1,samplesize=5)
+#must be "b" estimated with the real data for each case?, i.e. the value which generate the observed size difference
+simSeed<-function(N=100,n=10,b=0.005,ngen=1000,selq=0.2,sigma=1,samplesize=5) 
 {
     require(VGAM)
+    
                                         #Initialise Population
     genotype=matrix(NA,nrow=N,ncol=3) #genotype matrix
     genotype[,1]=rnorm(N,50,sigma) #first column contains randomly initialised means
@@ -10,26 +16,26 @@ simSeed<-function(N=100,n=10,b=0.05,ngen=1000,selq=0.2,sigma=1,samplesize=5)
     genotype[,3]=n #third column contains the number of offsprings
 
                                         # Define Archaeological Record
-
-   # record=matrix(NA,nrow=ngen,ncol=samplesize)
+    #record=matrix(NA,nrow=ngen,ncol=samplesize)
     recordedSDs<-numeric()
     recordedAvgs<-numeric()
                                         #Start Simulation
 
-    for (t in 1:ngen)
+    for (t in 1:ngen) #or 1:max(timeTable)
         {
                                         #generate seeds
             seeds=as.numeric(t(apply(genotype,1,function(x){replicate(x[3],rnorm(1,x[1],x[2]))})))
             index=rep(1:N,n) #record pointer to parent matrix
 
-            recordedSDs[t]=sd(seeds)
-            recordedAvgs[t]=mean(seeds)
+                                        #record 
+            recordedSDs[t]=sd(seeds) #sd
+            recordedAvgs[t]=mean(seeds) #mean
 
-                                        #random selection (archaeological sampling)
-            sample.index=sample(1:length(seeds),size=samplesize,replace=FALSE)
-            record[t,]=seeds[sample.index]
-            seeds=seeds[-sample.index] #remove from sample pool
-            index=index[-sample.index] #remove from sample pool
+                                        ##random selection (archaeological sampling)
+            #sample.index=sample(1:length(seeds),size=samplesize,replace=FALSE)
+            #record[t,]=seeds[sample.index]
+            #seeds=seeds[-sample.index] #remove from sample pool
+            #index=index[-saMple.index] #remove from sample pool
 
                                         #anthrophic selection
             
@@ -43,13 +49,52 @@ simSeed<-function(N=100,n=10,b=0.05,ngen=1000,selq=0.2,sigma=1,samplesize=5)
             genotype=genotype[sowdIndex,] #create new genotype
             
                                         #Mutation
-            genotype=genotype+rlaplace(nrow(genotype),0,scale=b)
+            genotype[,1]=genotype[,1]+rlaplace(nrow(genotype),0,scale=b) #mutation affected all gens (not anymore)
         }
 
-                                        #Place Holder Calculate Haldane
+                                        #Holder Calculate Haldane (without collapse)
 
-    # haldane = somefunction(recordedSDs,recordedAvgs)
-    
-    return(record) # return(list(raw=record,haldane=haldane))
+    haldanes=recordedAvgs/pooledSd(rep(n*N,ngen),recordedSDs) #dependent variable (y axis in the linear model)
+    Time=1:ngen #independent variable (x axis in the linear model)
+    haldaneCoef=summary(lm(haldanes~Time))$coefficients[2] # B coefficient (haldanes rate)
 
+
+    return(data.frame(haldane=haldaneCoef, N=N, selq=selq)) # return(list(raw=record,haldane=haldane))
 }
+
+                                        ### Collapse
+#in order to callpase the generations we have to define a arqueological record: a random sampling (if it is possible, with a 
+#different samplesize for each generation)
+
+                                        ### Main function
+#estimated execution time (CPU 2.93GHz DDR3 166 MHz): 90 min
+mainFunction <- function(ngen=2000, settings=data.frame(N=c(rep(50, 9), rep(100, 9), rep(150, 9)), selq=c(rep(seq(0.1, 0.9, 0.1), 3))), sims=10)
+{
+    require(ggplot2)
+
+    # define the result
+    Result <- data.frame()
+
+    # loop through simulations
+    for (i in 1:sims) 
+    {
+        results <- Reduce(function(...) rbind(...), apply(settings, 1, function(x) {simSeed(N=x[1], selq=x[2], ngen=ngen)}))
+        Result <- rbind(Result, results)
+    }
+
+    # ggplot
+    p <- ggplot(Result, aes(x=selq, y=haldane, group=N)) + 
+        #geom_point(aes(colour=factor(N)), size=1.5) + 
+        geom_smooth(aes(colour=factor(N))) + 
+        geom_hline(yintercept = 0.001)
+
+    return(p)
+}
+
+
+
+
+
+
+
+
